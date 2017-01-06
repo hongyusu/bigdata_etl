ec3f1e4ab96a5192ebbc562d33aeb7547786983a
Hongyu Su <hongyu.su@me.com>
2017-01-06 12:01:33 +0200

    add java kafka customer consumer

---
+++ streaming/src/main/java/streaming/KafkaCustomerConsumer.java
@@ -0,0 +1,34 @@
+
+
+package streaming;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class KafkaCustomerConsumer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "mygroup");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("test"));
+
+        boolean running = true;
+        while (running) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.value());
+            }
+        }
+
+        consumer.close();
+    }
+}
