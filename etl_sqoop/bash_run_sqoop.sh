1c0d73aff2b93858b1448eeeafbc2f77d11d3863
Hongyu Su <hongyu.su@me.com>
2017-03-01 22:08:00 +0200

    add sqoop table via sql query

--- etl_sqoop/bash_run_sqoop.sh
+++ etl_sqoop/bash_run_sqoop.sh
@@ -1,1 +1,26 @@
-sqoop import --connect jdbc:mysql://localhost/test --username root --P --table testtable --target-dir ./testtable -m 1
+
+
+# ingestion from mysql to hdfs : query
+
+sqoop import \
+    --connect jdbc:mysql://localhost/test \
+    --username root \
+    --P \
+    --query "select * from testtable where \$CONDITIONS" \
+    --target-dir ./testtable_query \
+    -m 1
+
+exit
+
+# ingestion from mysql to hdfs : whole table
+
+sqoop import \
+    --connect jdbc:mysql://localhost/test \
+    --username root \
+    --P \
+    --table testtable \
+    --target-dir ./testtable_table \
+    -m 1
+
+
+
