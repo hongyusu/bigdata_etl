9c2f5979be51458bffacbc8c9d51bd0f6d757714
Hongyu Su <hongyu.su@me.com>
2017-03-13 23:31:40 +0200

    storm word count example

--- etl_storm/src/main/java/etl_storm/WordCountTopology.java
+++ etl_storm/src/main/java/etl_storm/WordCountTopology.java
@@ -7,11 +7,14 @@ import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
 import org.apache.storm.task.ShellBolt;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.IRichBolt;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
@@ -21,22 +24,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WordCountTopology {
-    public static class SplitSentence extends ShellBolt implements IRichBolt {
-
-        public SplitSentence() {
-            //super("python", "/Users/hongyusu/Codes/bigdata_etl/storm_etl/splitsentence.py");
+    public static class SplitSentence extends BaseRichBolt {
+        OutputCollector _collector;
+        @Override
+        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+            _collector = collector;
+        }
+        @Override
+        public void execute(Tuple tuple) {
+            String[] words = tuple.getString(0).split(" ");
+            for(String word : words) {
+                _collector.emit(tuple, new Values(word));
+                _collector.ack(tuple);
+            }
         }
 
         @Override
         public void declareOutputFields(OutputFieldsDeclarer declarer) {
             declarer.declare(new Fields("word"));
         }
-
-        @Override
-        public Map<String, Object> getComponentConfiguration() {
-            return null;
-        }
     }
+
 
     public static class WordCount extends BaseBasicBolt {
         Map<String, Integer> counts = new HashMap<String, Integer>();
